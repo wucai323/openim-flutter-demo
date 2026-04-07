3c48a5670070fea183fa98e5843025675be5a3ee
wucai323 <wucai1688@gmail.com>
2026-04-07 18:53:23 +0800

    feat: add GeTui push implementation

--- openim_common/lib/src/controller/push_controller.dart
+++ openim_common/lib/src/controller/push_controller.dart
@@ -6,37 +6,24 @@ import 'package:get/get.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:google_api_availability/google_api_availability.dart';
 import 'package:openim_common/openim_common.dart';
+import 'package:getuiflut/getuiflut.dart';
 
 import 'firebase_options.dart';
 
-enum PushType { FCM, none }
+enum PushType { GeTui, FCM, none }
 
-const appID = 'your-app-id';
-const appKey = 'your-app-key';
-const appSecret = 'your-app-secret';
+const appID = 'ovKF2KkTmC99F7luUa2T06';
+const appKey = 'bFUTJhLCkr7pfTWvJ0UCY4';
+const appSecret = '5sLKQk42zxAMUjJHWETRr7';
 
 class PushController extends GetxService {
-  PushType pushType = PushType.none;
+  PushType pushType = PushType.GeTui;
 
   /// Logs in the user with the specified alias to the push notification service.
-  ///
-  /// Depending on the push type configured, it either logs in using the Getui or
-  /// FCM push service.
-  ///
-  /// If using Getui, it binds the alias to the Getui service.
-  ///
-  /// If using FCM, it listens for token refresh events and logs in, invoking the
-  /// provided callback with the new token.
-  ///
-  /// Throws an assertion error if the FCM push type is selected but the
-  /// `onTokenRefresh` callback is not provided.
-  ///
-  /// - Parameters:
-  ///   - alias: The alias to bind to the push notification service for getui.
-  ///   - onTokenRefresh: A callback function that is invoked with the refreshed
-  ///     token when using FCM. Required if the push type is FCM.
   static void login(String alias, {void Function(String token)? onTokenRefresh}) {
-    if (PushController().pushType == PushType.FCM) {
+    if (PushController().pushType == PushType.GeTui) {
+      GetuiPushController()._initialize(alias, onTokenRefresh);
+    } else if (PushController().pushType == PushType.FCM) {
       assert((PushController().pushType == PushType.FCM && onTokenRefresh != null));
 
       FCMPushController()._initialize().then((_) {
@@ -47,8 +34,70 @@ class PushController extends GetxService {
   }
 
   static void logout() {
-    if (PushController().pushType == PushType.FCM) {
+    if (PushController().pushType == PushType.GeTui) {
+      GetuiPushController()._unbindAlias();
+    } else if (PushController().pushType == PushType.FCM) {
       FCMPushController()._deleteToken();
+    }
+  }
+}
+
+class GetuiPushController {
+  static final GetuiPushController _instance = GetuiPushController._internal();
+  factory GetuiPushController() => _instance;
+
+  GetuiPushController._internal();
+
+  Getuiflut? _getuiflut;
+
+  Future<void> _initialize(String alias, void Function(String token)? onTokenRefresh) async {
+    _getuiflut = Getuiflut();
+    
+    await _getuiflut!.initGetuiSdk(
+      appId: appID,
+      appKey: appKey,
+      appSecret: appSecret,
+    );
+
+    // Get ClientID
+    String? clientId = await _getuiflut!.getClientId();
+    Logger.print("GeTui ClientID: $clientId");
+    
+    if (clientId != null && onTokenRefresh != null) {
+      onTokenRefresh(clientId);
+    }
+
+    // Bind alias
+    if (alias.isNotEmpty) {
+      await _getuiflut!.bindAlias(alias, clientId ?? '');
+      Logger.print("GeTui alias bound: $alias");
+    }
+
+    // Listen for ClientID registration
+    _getuiflut!.addEventHandler(
+      onReceiveClientId: (String clientId) {
+        Logger.print("GeTui ClientID received: $clientId");
+        if (onTokenRefresh != null) {
+          onTokenRefresh(clientId);
+        }
+      },
+      onReceiveMessageData: (Map<String, dynamic> message) {
+        Logger.print("GeTui message received: $message");
+      },
+      onNotificationMessageArrived: (Map<String, dynamic> message) {
+        Logger.print("GeTui notification arrived: $message");
+      },
+      onNotificationMessageClicked: (Map<String, dynamic> message) {
+        Logger.print("GeTui notification clicked: $message");
+      },
+    );
+  }
+
+  Future<void> _unbindAlias() async {
+    String? clientId = await _getuiflut?.getClientId();
+    if (clientId != null) {
+      await _getuiflut?.unbindAlias(clientId, true);
+      Logger.print("GeTui alias unbound");
     }
   }
 }
@@ -82,12 +131,12 @@ class FCMPushController {
 
   Future<void> _requestPermission() async {
     NotificationSettings settings = await FirebaseMessaging.instance.requestPermission();
-    print('User granted permission: ${settings.authorizationStatus}');
+    print('User granted permission: \${settings.authorizationStatus}');
   }
 
   void _configureForegroundNotification() {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
-      print('Foreground notification received: ${message.notification?.title}');
+      print('Foreground notification received: \${message.notification?.title}');
 
       if (message.notification != null) {}
     });
@@ -95,12 +144,12 @@ class FCMPushController {
 
   void _configureBackgroundNotification() {
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('App opened from background: ${message.notification?.title}');
+      print('App opened from background: \${message.notification?.title}');
     });
 
     FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? message) {
       if (message != null) {
-        print('App opened from terminated state: ${message.notification?.title}');
+        print('App opened from terminated state: \${message.notification?.title}');
       }
     });
   }
